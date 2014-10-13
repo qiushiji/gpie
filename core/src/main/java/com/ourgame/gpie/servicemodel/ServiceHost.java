4779a39b48c049f0262d70a39e83b97723ea498e
qiushi <qiushiji@hotmail.com>
2014-10-13 19:18:10 +0800

    write some sample codes

--- core/src/main/java/com/ourgame/gpie/servicemodel/ServiceHost.java
+++ core/src/main/java/com/ourgame/gpie/servicemodel/ServiceHost.java
@@ -1,23 +1,55 @@
 package com.ourgame.gpie.servicemodel;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
 public class ServiceHost extends ServiceObject {
+
+	private URI address;
+	private String name;
+
+	private NioSocketAcceptor acceptor;
+
+	public ServiceHost(String name) {
+		this(name, null);
+	}
+
+	public ServiceHost(String name, URI address) {
+		this.name = name;
+		this.address = address;
+
+		this.initialize();
+	}
+
+	private void initialize() {
+		this.acceptor = new NioSocketAcceptor();
+	}
 
 	@Override
 	public long getDefaultOpenTimeout() {
-		return 0;
+		return 30L;
 	}
 
 	@Override
 	public long getDefaultCloseTimeout() {
-		return 0;
+		return 30L;
 	}
 
 	@Override
 	protected void onOpen(long timeout) {
+		try {
+			this.acceptor.bind(new InetSocketAddress(this.address.getPath(), this.address.getPort()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	protected void onClose(long timeout) {
+		this.acceptor.dispose(true);
 	}
 
 	@Override
--- sample/src/main/java/com/ourgame/gpie/sample/HelloWorld.java
+++ sample/src/main/java/com/ourgame/gpie/sample/HelloWorld.java
@@ -1,5 +1,31 @@
 package com.ourgame.gpie.sample;
 
-public class HelloWorld {
+import java.lang.reflect.Method;
+import java.net.URI;
 
+public class HelloWorld {
+	public static void main(String[] args) throws Exception {
+
+		long start = System.currentTimeMillis();
+		URI uri = new URI("tcp://qiushi:hello@localhost:8888");
+
+		Method[] methods = uri.getClass().getMethods();
+		for (int i = 0; i < methods.length; ++i) {
+
+			Method method = methods[i];
+
+			Class<?> retType = method.getReturnType();
+
+			if (method.getName().startsWith("get")
+					&& (retType.getSimpleName().equals("String") || retType.getSimpleName().equals("int"))) {
+				Object ret = method.invoke(uri, (Object[]) null);
+
+				System.out.printf("%s : %s \n", method.getName(), (ret == null) ? null : ret.toString());
+			}
+		}
+
+		long end = System.currentTimeMillis();
+
+		System.out.printf("cost %d\n", end - start);
+	}
 }
